3a3caa149677d6c54105dfd2d45a3ec75d51d1fa
dhd <dhd@7a6dc53a-678a-4e31-bf73-3c095f94da06>
2012-11-06 15:11:45 +0000

    git-svn-id: svn+ssh://green.exnet.com/rw/SVN-repositories/public/reutils/trunk@317 7a6dc53a-678a-4e31-bf73-3c095f94da06

--- javasrc/org/hd/d/edh/RemoteGenerationIntensity.java
+++ javasrc/org/hd/d/edh/RemoteGenerationIntensity.java
@@ -4,8 +4,12 @@ import java.io.IOException;
 
 /**Get live generation intensity of remote grid.
  * Used for estimating intensity of inbound/imported energy across an interconnector.
+ * <p>
+ * Instances may not be internally thread-safe
+ * but separate instances should be safe to run in separate threads,
+ * eg to gather date on several remote grids concurrently.
  */
-public interface LiveRemoteIntensity
+public interface RemoteGenerationIntensity
     {
     /**Gets remote grid human-readable name/abbreviation; never null nor empty. */
     public String gridName();
@@ -13,6 +17,8 @@ public interface LiveRemoteIntensity
     /**Get latest intensity in gCO2/MWh; non-negative.
      * Attempts to get recent intensity,
      * or throws an exception if no reasonably recent value available, eg within last hour or two.
+     * <p>
+     * May take a while to run, so may often by run in a thread.
      *
      * @return latest available recent/current generation intensity (gCO2/MWh)
      * @throws IOException if no sufficiently-recent or reliable value is available
