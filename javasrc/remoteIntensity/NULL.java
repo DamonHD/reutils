9c3e6ecdeddd511c3b0e74f4de8bf5279d97d78a
dhd <dhd@7a6dc53a-678a-4e31-bf73-3c095f94da06>
2012-11-06 15:13:57 +0000

    git-svn-id: svn+ssh://green.exnet.com/rw/SVN-repositories/public/reutils/trunk@318 7a6dc53a-678a-4e31-bf73-3c095f94da06

---
+++ javasrc/remoteIntensity/NULL.java
@@ -0,0 +1,12 @@
+package remoteIntensity;
+
+import java.io.IOException;
+
+import org.hd.d.edh.RemoteGenerationIntensity;
+
+/**NULL remote-intensity fetcher; never succeeds. */
+public final class NULL implements RemoteGenerationIntensity
+    {
+    @Override public String gridName() { return("NULL"); }
+    @Override public int getLatest() throws IOException { throw new IOException(); }
+    }
